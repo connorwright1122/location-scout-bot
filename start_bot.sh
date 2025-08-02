bdcd5472059225c5adce582e6297260c0bf65e46
connorwright1122 <connorwright1122@gmail.com>
2025-08-02 14:08:33 -0400

    stash before pull

--- start_bot.sh
+++ start_bot.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-#sleep 30
 ## make sure that the bot has access to write to bot.log with sudo chmod 666 /home/connorwright/Downloads/location-scout-bot/bot.log
 
 echo "===== Starting bot at $(date) =====" >> /home/connorwright/Downloads/location-scout-bot/bot.log
 
 cd /home/connorwright/Downloads/location-scout-bot || exit
 
+# Stash local changes before pulling
+echo "Stashing local changes..." >> "$LOGFILE"
+/usr/bin/git stash --include-untracked >> "$LOGFILE" 2>&1
+
 # Pull updates and log result
 echo "Running git pull..." >> bot.log
 /usr/bin/git pull >> bot.log 2>&1
+
+sleep 10
 
 # Activate venv and start bot
 echo "Launching bot.py..." >> bot.log
